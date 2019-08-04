a0633a359fce0e8fb6305e4caac1315eed266cec
Ondřej Martínek <46747310+GeorgePowhattan@users.noreply.github.com>
2019-08-04 15:35:34 +0200

    add

---
+++ 3 Find the non-duplicate number.py
@@ -0,0 +1,22 @@
+# Given a list of numbers, where every number shows up twice except for one number, find that one number.
+
+'''Example:
+Input: [4, 3, 2, 4, 1, 3, 2]
+Output: 1'''
+
+# Solution:
+def singleNumber(nums):
+    result = []
+    for number in lst:
+        if lst.count(number) == 1:
+            result.append(number)
+    return result
+
+# Solution using O(1) memory:
+def singleNumberConst(nums):
+    for number in lst:
+        
+
+lst = [4, 3, 2, 4, 1, 3, 2, 4]
+
+print(singleNumber(lst))
