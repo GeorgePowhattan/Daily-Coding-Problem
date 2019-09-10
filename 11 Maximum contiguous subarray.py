a74af96eaca232fd9c09e66b8ccc2427574e0807
Ondřej Martínek <46747310+GeorgePowhattan@users.noreply.github.com>
2019-09-10 20:02:37 +0200

    change

--- 11 Maximum contiguous subarray.py
+++ 11 Maximum contiguous subarray.py
@@ -1,3 +1,11 @@
+'''You are given an array of integers. Find the maximum sum of all possible contiguous subarrays of the array.
+
+Example:
+
+[34, -50, 42, 14, -5, 86]
+
+Given this input array, the output should be 137. The contiguous subarray with the largest sum is [42, 14, -5, 86].'''
+
 def maxSequence(arr):
     if all(element < 0 for element in arr):
         return 0
