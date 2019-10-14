65899c7638f3d82f359b7417e55b3cc042cd94a9
Ondřej Martínek <46747310+GeorgePowhattan@users.noreply.github.com>
2019-10-14 14:08:22 +0200

    add

---
+++ Daily Coding Problem/15 Roman numurals.py
@@ -0,0 +1,30 @@
+# Convert Roman Numerals to Decimal
+
+hashmap = dict(
+I     =1,
+V     =5, 
+X     =10,
+L     =50,
+C     =100,
+D     =500,
+M     =1000)
+
+n = 'MCMX'
+
+def romanToInt(string):
+    res = 0
+    i = 0
+    while i < len(string):    # check if the number after is greater than the current one - subtracting
+        if i+1 < len(string):
+            if hashmap[string[i+1]] > hashmap[string[i]]:
+                res = res + hashmap[string[i+1]] - hashmap[string[i]]
+                i += 2
+            else:
+                res += hashmap[string[i]]
+                i += 1
+        else:
+            res += hashmap[string[i]]
+            i += 1
+    return res
+
+print(romanToInt(n))
