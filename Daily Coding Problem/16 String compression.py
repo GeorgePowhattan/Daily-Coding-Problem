07b42e41ee40ac59bfa5c3b58c90bb1e72c36fe3
Ondřej Martínek <46747310+GeorgePowhattan@users.noreply.github.com>
2019-10-15 18:32:54 +0200

    add

--- Daily Coding Problem/16 String compression.py
+++ Daily Coding Problem/16 String compression.py
@@ -1,3 +1,9 @@
+# Given an array of characters with repeats, compress it in place. The length after compression should be less than or equal to the original array.
+
+'''Example:
+Input: ['a', 'a', 'b', 'c', 'c', 'c']
+Output: ['a', '2', 'b', 'c', '3']'''
+
 def compress(chars):
     output = []
     previous = None
@@ -16,3 +22,7 @@ def compress(chars):
     if counter > 1:
         output.append(str(counter))
     return output
+
+lst = ['a', 'a', 'b', 'c', 'c', 'c']
+
+print(compress(lst))
