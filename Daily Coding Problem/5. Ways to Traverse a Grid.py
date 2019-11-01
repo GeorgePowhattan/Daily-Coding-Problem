b10083e118cf2864f4dd0293e8ae344702b37d32
GeorgePowhattan <martinek.ondrej@gmail.com>
2019-11-01 10:15:57 +0100

    add recursion

--- Daily Coding Problem/5. Ways to Traverse a Grid.py
+++ Daily Coding Problem/5. Ways to Traverse a Grid.py
@@ -13,6 +13,17 @@ def num_ways(n, m):
     comb = (math.factorial(n+m-2) / (math.factorial(m-1) * math.factorial(n-1)))
     return int(comb)
 
-print(num_ways(2, 2))
+print(num_ways(4, 3))
 # 2
 
+ 
+# ----------------------------------------------------------------------------------
+
+# Recursive computation
+
+def num_ways_rec(n,m):
+    if n == 1 or m == 1:
+        return 1
+    return num_ways_rec(n-1,m) + num_ways_rec(n,m-1)
+
+print(num_ways_rec(3, 3))
