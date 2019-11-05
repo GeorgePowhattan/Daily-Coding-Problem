1dfb1c896722e97c61cd7e38978e625dc669817e
Ondřej Martínek <46747310+GeorgePowhattan@users.noreply.github.com>
2019-11-05 12:03:24 +0100

    Update 11. Maximum contiguous subarray.py

--- Daily Coding Problem/11. Maximum contiguous subarray.py
+++ Daily Coding Problem/11. Maximum contiguous subarray.py
@@ -6,16 +6,41 @@ Example:
 
 Given this input array, the output should be 137. The contiguous subarray with the largest sum is [42, 14, -5, 86].'''
 
-def maxSequence(arr):
+# Brute force solution
+def max_subarray_sum(arr):
+    sum_arr = []
     if all(element < 0 for element in arr):
         return 0
     else:
-        current_max = 0
-        for i in range(len(arr)):
-            for j in range(i,len(arr)+1):
-               current_max = max(current_max, sum(arr[i:j]))
-        return(current_max)
+        for i in range(len(a)):
+            for j in range (i+1, len(a)+1):
+                sum_arr.append(sum(a[i:j]))
+    return max(sum_arr)
 
-arr = [34, -50, 42, 14, -5, 86]
+# Better solution running in linear time:
+def max_subarray_sum_linear(arr):
+    max_sum_arr = 0
+    sum_arr = 0
+    for number in arr:
+        if number >= 0:
+            sum_arr += number
+            max_sum_arr = max(max_sum_arr, sum_arr)
+        else:
+            sum_arr += number
+            sum_arr = max(0, sum_arr) 
+    return max_sum_arr
+    
+b = [random.randint(-5,10) for i in range(0,10)]
+c = [random.randint(-5,10) for i in range(0,10)]
+d = [random.randint(-5,10) for i in range(0,10)]
+print(b)
+print(c)
+print(d)
 
-print(maxSequence(arr))
+# Test both solutions: 
+print(max_subarray_sum(b))
+print(max_subarray_sum(c))
+print(max_subarray_sum(d))
+print(max_subarray_sum_linear(b))
+print(max_subarray_sum_linear(c))
+print(max_subarray_sum_linear(d))
