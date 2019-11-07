08897e5949eac1df9a1b45b7770cabfcf4723eba
Ondřej Martínek <46747310+GeorgePowhattan@users.noreply.github.com>
2019-11-07 19:18:19 +0100

    change

--- Daily Coding Problem/21. Longest Consecutive.py
+++ Daily Coding Problem/21. Longest Consecutive.py
@@ -1,6 +1,6 @@
-'''Longest Consecutive SequenceYou are given an array of integers.
+'''Longest Consecutive Sequence
 
-Return the length of the longest consecutive elements sequence in the array.
+You are given an array of integers. Return the length of the longest consecutive elements sequence in the array.
 
 For example, the input array [100, 4, 200, 1, 3, 2] has the longest consecutive sequence 1, 2, 3, 4, and thus, you should return its length, 4.'''
 
--- Daily Coding Problem/8. Maximum Profit From Stocks.py
+++ Daily Coding Problem/8. Maximum Profit From Stocks.py
@@ -20,3 +20,24 @@ def buy_and_sell(arr):
 
 print (buy_and_sell([9, 11, 8, 5, 7, 10]))
 # 5
+
+# ---------------------------------------------------------------------------------------------------------------
+# Another solution 
+
+def profit_from_stocks(prices):
+    profit = 0
+    maxprofit = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i-1]:
+            profit += prices[i]-prices[i-1]
+        else:
+            profit = max(0, profit + prices[i] - prices[i-1])
+        
+        maxprofit = max(maxprofit, profit)
+    return maxprofit
+    
+a = [9,11,8,5,7,16,17,18,1,2,3]
+b = [1,2,3,4,2,3,4,5,6,7,3,4]
+c = [10,11,12,1,2,3,4,6,7,0,7]
+
+print(profit_from_stocks(c))
