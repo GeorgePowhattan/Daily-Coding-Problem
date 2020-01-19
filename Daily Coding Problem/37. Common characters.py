3e64acacd296eb451d1194fc8cd342ccd5c5bca7
Ondřej Martínek <46747310+GeorgePowhattan@users.noreply.github.com>
2020-01-19 21:25:18 +0100

    Update 37. Common characters.py

--- Daily Coding Problem/37. Common characters.py
+++ Daily Coding Problem/37. Common characters.py
@@ -1,13 +1,14 @@
 # Given a list of strings, find the list of characters that appear in all strings.
 
-def common_characters(strs):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    # create alphabet hashtable
     charmap = {char: 0 for char in alphabet }
+    # loop through set of characters in each word
     for word in strs:
-        for char in word:
+        for char in set(word):
             charmap[char] += 1
 
-    return [key for key in charmap.keys() if charmap[key] == 3]
+    return [key for key in charmap.keys() if charmap[key] == len(strs)]
     
 print(common_characters(['google', 'facebook', 'youtube']))
 # ['e', 'o']
